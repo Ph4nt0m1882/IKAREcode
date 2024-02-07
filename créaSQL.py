3bdccf11ebb501eb3b9bc025551ebc40db77cdd7
Phantom <107296563+Ph4nt0m1882@users.noreply.github.com>
2024-02-07 23:56:03 +0100

    Update créaSQL.py

--- créaSQL.py
+++ créaSQL.py
@@ -1,2 +1,6 @@
 import sqlite3, os
 dir=os.path.dirname(os.path.abspath(__file__))
+
+conn=sqlite3.connect('https://github.com/Ph4nt0m1882/IKAREcode/blob/main/AsDBCoIn.db')
+cur=conn.cursor()
+conn.close()
